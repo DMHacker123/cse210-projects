e2718f3dc02c3b07f461120f95e449c1b4c6d0fb
DMHacker123 <matosalvarezdm@hotmail.com>
2025-07-21 22:11:58 -0400

    week4

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -17,7 +17,7 @@ class Program
         job2._endYear = 2023;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Darwin Matos";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
